9514dc35f9f125e7775b226a1d0555a3ff230e42
koleq(WORK-D) <o.kurz@atlas.cz>
2022-06-07 11:50:48 +0200

    add multifolder backup, fix error and logs

--- backup.rb
+++ backup.rb
@@ -10,9 +10,10 @@ log_location = "#{path}log/"
 
 ####### přečíst stanice a zapsat je ######
 # stations[0][0] = stanice 1 IP
-# stations[0][1] = stanice 1 jméno uživatele
-# stations[0][2] = stanice 1 heslo uživatele
-# stations[0][3] = cesta která se má zálohovat
+# stations[0][1] = stanice 1 share
+# stations[0][2] = stanice 1 jméno uživatele
+# stations[0][3] = stanice 1 heslo uživatele
+# stations[0][4] = cesta která se má zálohovat
 File.readlines("#{path}stations.csv").each { |line|
     station = line.encode('UTF-8', :invalid => :replace).delete("\n").split(";")
     stations.append(station)
@@ -20,21 +21,24 @@ File.readlines("#{path}stations.csv").each { |line|
 
 year_week = Time.now.strftime("%Y-%W") # Rok a týden
 
+system("del /Q #{err_location.gsub("/","\\")}*.*")
+system("del /Q #{log_location.gsub("/","\\")}*.*")
+
 ####### vytvoření backupů ######
 stations.each_with_index { |station, i|
     if system("ping -w 800 #{station[0]} -n 4")  # backup only if station is online
-        station[3] = "\"#{station[3]}\""  # přidání uvozovek
+        #station[4] = "\"#{station[4]}\""  # přidání uvozovek
 
         puts("\n ==== připojit disk ====")
-        connect = "net use Q: \\\\#{station[0]}\\c$"
-        if station[1] != ""  # pokud je uvedené jméno
-            connect = connect +  " /user:#{station[1]} #{station[2]}"
+        connect = "net use Q: \\\\#{station[0]}\\#{station[1]}"
+        if station[2] != ""  # pokud je uvedené jméno
+            connect = connect +  " /user:#{station[2]} #{station[3]}"
         end
         system(connect) # připojit síťový disk
 
         puts("==== start archivace ====")
         system("#{path}programy/7z2107_x64/7za.exe u -r -up1q0r2x1y2z1w2 -y -bb #{backup_location}backup_#{station[0]}_#{year_week}.7z " +
-               "#{station[3]} 1> #{log_location}log-#{station[0]}.txt 2> #{err_location}err-#{station[0]}.txt")  # vytvořit backup
+               "#{station[4]} 1> #{log_location}log-#{station[0]}.txt 2> #{err_location}err-#{station[0]}.txt")  # vytvořit backup
         puts("==== konec archivace ===")
 
         puts("\n ==== odpojit disk ====")
